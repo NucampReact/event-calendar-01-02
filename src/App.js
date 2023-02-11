303fde9808408e056e7da98ba83d7f211ccc6a40
Nas Baki <nas.baki@holman.com>
2023-02-11 12:20:49 -0500

    Project setup and intro

--- src/App.js
+++ src/App.js
@@ -1,23 +1,56 @@
 import logo from './logo.svg';
 import './App.css';
 
+/*
+  JSX (JavaScript XML): Markup language that translates XML into JavaScript then into HTML
+
+  How does this JSX render onto our screen?
+    1. Create XML markup (like HTML syntax) 
+    2. Create JS commands needed
+    3. Create HTML tags to render onto HTML document
+
+    App(); // Invoke the App function
+*/
+
+/*
+  Props (properties) = data that you can pass into your function, from one
+    component to another
+  
+  Props is read-only object of data
+
+  {
+    msg: 'Welcome',
+    foo: 'bar',
+    num: 1
+  }
+*/
+
+
+
+const messages = ['Welcome to my application!', 'Get started by clicking Shop Now', 'Talk to an agent today'];
+
+/*
+  [
+    <Subtitle />,
+    <Subtitle />,
+    <Subtitle />
+  ]
+*/
+
+function Subtitle(props) {
+  return (
+    <h2>{props.msg}</h2>
+  )
+}
+
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>Event Calendar Application</h1>
+      {messages.map((message, index) => {
+        // Each child in a list should have a unique "key" prop.
+        return <Subtitle key={index} msg={message} /> // create a subtitle component and pass to it our message
+      })}
     </div>
   );
 }
